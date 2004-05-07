f73d014c49ecda8fbf671cb1f00b1fa0ad19a9ae
krys <>
2004-05-07 17:58:22 +0000

    - added GetConfigurationString() - moved some GetXXX to KD_Application

--- krystaldrop/Sources/KDpp/Tools/XMLConfig.cpp
+++ krystaldrop/Sources/KDpp/Tools/XMLConfig.cpp
@@ -9,246 +9,197 @@
 
 KD_XMLConfig::KD_XMLConfig()
 {
-	configDocument = 0;
-	rootNode = 0;
+    configDocument = 0;
+    rootNode = 0;
 }
 
 KD_XMLConfig::~KD_XMLConfig()
 {
-	Unload();
+    Unload();
 }
 
 bool KD_XMLConfig::Load()
 {
-	char user_file[128];
+    char user_file[128];
 #ifndef WIN32
-	/* find the user's home, the UNIX way */
-	char* home= getenv("HOME");
+    /* find the user's home, the UNIX way */
+    char* home= getenv("HOME");
 #else
-	/* find the user's home, the Windoze way */
-	/* Document Settings\username\ ? */
-	// Pfff too hard under windows: what the f... name of My Documents in Polish for example? hu?
-	// k: if you do not know, i wont :p
-	//string hometemp = getenv("USERPROFILE");
-	char* home= ".";
+    /* find the user's home, the Windoze way */
+    /* Document Settings\username\ ? */
+    // Pfff too hard under windows: what the f... name of My Documents in Polish for example? hu?
+    // k: if you do not know, i wont :p
+    //string hometemp = getenv("USERPROFILE");
+    char* home= ".";
 #endif
 
-	if (home != NULL)
-	{
-		// try $(HOME)/kdrop.xml
-		snprintf (user_file, 128, "%s/kdrop.xml", home);
-		user_file[127]= 0;
-		if (Load(user_file)== true)
-			return true;
+    if (home != NULL)
+    {
+        // try $(HOME)/kdrop.xml
+        snprintf (user_file, 128, "%s/kdrop.xml", home);
+        user_file[127]= 0;
+        if (Load(user_file)== true)
+            return true;
 
-		// try $(HOME)/.kdroprc
-		snprintf (user_file, 128, "%s/.kdroprc", home);
-		user_file[127]= 0;
-		if (Load(user_file)== true)
-			return true;
-	}
+        // try $(HOME)/.kdroprc
+        snprintf (user_file, 128, "%s/.kdroprc", home);
+        user_file[127]= 0;
+        if (Load(user_file)== true)
+            return true;
+    }
 
-	// try ./kdrop.xml
-	if (Load("kdrop.xml")==true)
-		return true;
+    // try ./kdrop.xml
+    if (Load("kdrop.xml")==true)
+        return true;
 
 #ifndef WIN32
-	/* for UNIXes system, try CFGDIR/kdrop.xml */
-	snprintf (user_file, 128, "%s/kdrop.xml", CFGDIR);
-	user_file[127]= 0;
-	if (Load(user_file)== true)
-		return true;
+    /* for UNIXes system, try CFGDIR/kdrop.xml */
+    snprintf (user_file, 128, "%s/kdrop.xml", CFGDIR);
+    user_file[127]= 0;
+    if (Load(user_file)== true)
+        return true;
 #endif
 
-	// everything failed :(
-	return false;
+    // everything failed :(
+    return false;
 }
 
 bool KD_XMLConfig::Load(const KD_FilePath &xmlFileName)
 {
-	// First, try to open the file
-	FILE *fpt = fopen (xmlFileName.GetFullPath().c_str(), "r");
-	if (fpt == NULL)
-	  return false;
-	else
-	  fclose(fpt);
-	  
-	// ok we are here so the file does exist.
-	// let's open it with a real URI
-	configDocument = xmlParseFile(xmlFileName.GetFullPath().c_str());
+    // First, try to open the file
+    FILE *fpt = fopen (xmlFileName.GetFullPath().c_str(), "r");
+    if (fpt == NULL)
+      return false;
+    else
+      fclose(fpt);
 
-	if (configDocument == NULL)
-		return false;
+    // ok we are here so the file does exist.
+    // let's open it with a real URI
+    configDocument = xmlParseFile(xmlFileName.GetFullPath().c_str());
 
-	rootNode = xmlDocGetRootElement(configDocument);
+    if (configDocument == NULL)
+        return false;
 
-	if (rootNode == NULL)
-	{
-		KD_LogFile::printf2("Warning! Could not parse config file %s: no root node in XML document.\n",xmlFileName.GetFullPath().c_str());
-		xmlFreeDoc(configDocument);
-		return false;
-	}
+    rootNode = xmlDocGetRootElement(configDocument);
 
-	if (xmlStrcmp(rootNode->name, (const xmlChar *) "config"))
-	{
-		KD_LogFile::printf2("Warning! Root node of file %s is not of type \"config\".\nTrying to continue parsing.\n",xmlFileName.GetFullPath().c_str());
-	}
+    if (rootNode == NULL)
+    {
+        KD_LogFile::printf2("Warning! Could not parse config file %s: no root node in XML document.\n",xmlFileName.GetFullPath().c_str());
+        xmlFreeDoc(configDocument);
+        return false;
+    }
 
-	return true;
+    if (xmlStrcmp(rootNode->name, (const xmlChar *) "config"))
+    {
+        KD_LogFile::printf2("Warning! Root node of file %s is not of type \"config\".\nTrying to continue parsing.\n",xmlFileName.GetFullPath().c_str());
+    }
+
+    return true;
 }
 
 bool KD_XMLConfig::Unload()
 {
-	if (configDocument)
-	{
-		xmlFreeDoc(configDocument);
-		configDocument=0;
-	}
-	return true;
+    if (configDocument)
+    {
+        xmlFreeDoc(configDocument);
+        configDocument=0;
+    }
+    return true;
 }
 
-xmlDocPtr KD_XMLConfig::GetConfigDocument()
+xmlDocPtr KD_XMLConfig::GetConfigDocument() const
 {
-	return configDocument;
+    return configDocument;
 }
 
-xmlNodePtr KD_XMLConfig::GetRootNode()
+xmlNodePtr KD_XMLConfig::GetRootNode() const
 {
-	return rootNode;
+    return rootNode;
 }
 
 xmlNodePtr KD_XMLConfig::FindFirstByName(xmlNodePtr node, string name)
 {
-	assert(node);
+    assert(node);
 
-	xmlNodePtr child;
-	child = node->children;
-	while (child != NULL)
-	{
-		if ((!xmlStrcmp(child->name, (const xmlChar *)name.c_str())))
-		{
-			return child;
-		}
-		 
-		child = child->next;
-	}
-	return NULL;
+    xmlNodePtr child;
+    child = node->children;
+    while (child != NULL)
+    {
+        if ((!xmlStrcmp(child->name, (const xmlChar *)name.c_str())))
+        {
+            return child;
+        }
+
+        child = child->next;
+    }
+    return NULL;
 }
 
-string KD_XMLConfig::GetNodeText(xmlNodePtr node)
+string KD_XMLConfig::GetNodeText(xmlNodePtr node) const
 {
-	xmlChar *key = xmlNodeListGetString(configDocument, node->xmlChildrenNode, 1);
-	string txt = (char *)key;
+    xmlChar *key = xmlNodeListGetString(configDocument, node->xmlChildrenNode, 1);
+    string txt = (char *)key;
     xmlFree(key);
- 	return txt;
+    return txt;
 }
 
-string KD_XMLConfig::GetAttributeFromNode(xmlNodePtr xmlnode, const string &name)
+string KD_XMLConfig::GetAttributeFromNode(xmlNodePtr xmlnode, const string &name) const
 {
-	xmlAttrPtr prop = xmlnode->properties;
+    xmlAttrPtr prop = xmlnode->properties;
 
-	for (; prop!=0; prop = prop->next)
-	{
-		if (xmlStrEqual(prop->name, reinterpret_cast<const xmlChar*>(name.c_str())))
-		{
-			xmlChar *key = xmlNodeListGetString(configDocument, prop->xmlChildrenNode, 1);
-			string txt = (char *)key;
-			xmlFree(key);
- 			return txt;
-		}
-	}
-	return "";
+    for (; prop!=0; prop = prop->next)
+    {
+        if (xmlStrEqual(prop->name, reinterpret_cast<const xmlChar*>(name.c_str())))
+        {
+            xmlChar *key = xmlNodeListGetString(configDocument, prop->xmlChildrenNode, 1);
+            string txt = (char *)key;
+            xmlFree(key);
+            return txt;
+        }
+    }
+    return "";
+}
+
+string KD_XMLConfig::GetConfigurationString (string config_item) const
+{
+    xmlNodePtr pCurrentNode = rootNode;
+    while (pCurrentNode)
+    {
+        string::size_type i = config_item.find('/');
+        if (i == string::npos)
+        {
+            xmlNodePtr p = FindFirstByName (pCurrentNode, config_item);
+            GetNodeText (p);
+            return GetNodeText(FindFirstByName(pCurrentNode, config_item));
+        }
+
+        // the slash is ignored if it is the first character
+        if (i)
+        {
+            // update the XML node
+            string prefix = config_item.substr (0, i);
+            pCurrentNode = FindFirstByName(pCurrentNode, prefix);
+            config_item = config_item.substr (i+ 1, config_item.length());
+        }
+    }
+
+    return "";
 }
 
 bool KD_XMLConfig::SetAttributeFromNode(xmlNodePtr xmlnode, const string &name, const string &value)
 {
-	xmlSetProp(xmlnode, reinterpret_cast<const xmlChar*>(name.c_str()) , reinterpret_cast<const xmlChar*>(value.c_str()));
-		
-	return true;
+    xmlSetProp(xmlnode, reinterpret_cast<const xmlChar*>(name.c_str()) , reinterpret_cast<const xmlChar*>(value.c_str()));
+
+    return true;
 }
 
 bool KD_XMLConfig::Save(const KD_FilePath &xmlFileName)
 {
-	if (!xmlFileName.IsArchived())
-	{
-		xmlSaveFormatFile (xmlFileName.GetFullPath().c_str(), configDocument, 1);
-		return true;
-	}
-	else
-		return false;
-}
-
-bool KD_XMLConfig::GetOpenGL()
-{
-	assert(rootNode);
-
-	string resStr = GetNodeText(FindFirstByName(FindFirstByName(FindFirstByName(rootNode, "global"),"video"),"opengl"));
-	
-	if (resStr[0]=='Y' || resStr[0]=='y')
-		return true;
-	else 
-		return false;
-}
-
-bool KD_XMLConfig::GetFullScreen()
-{
-	assert(rootNode);
-
-	string resStr = GetNodeText(FindFirstByName(FindFirstByName(FindFirstByName(rootNode, "global"),"video"),"fullscreen"));
-	
-	if (resStr[0]=='Y' || resStr[0]=='y')
-		return true;
-	else 
-		return false;
-}
-
-bool KD_XMLConfig::GetEnableSound()
-{
-	assert(rootNode);
-
-	string resStr = GetNodeText(FindFirstByName(FindFirstByName(FindFirstByName(rootNode, "global"),"sound"),"enable"));
-	
-	if (resStr[0]=='Y' || resStr[0]=='y')
-		return true;
-	else 
-		return false;
-}
-
-int KD_XMLConfig::GetSoundFrequency()
-{
-	assert(rootNode);
-
-	string resStr = GetNodeText(FindFirstByName(FindFirstByName(FindFirstByName(rootNode, "global"),"sound"),"frequency"));
-	
-	return atoi(resStr.c_str());
-}
-
-int KD_XMLConfig::GetSoundBits()
-{
-	assert(rootNode);
-
-	string resStr = GetNodeText(FindFirstByName(FindFirstByName(FindFirstByName(rootNode, "global"),"sound"),"bits"));
-	
-	return atoi(resStr.c_str());
-}
-
-bool KD_XMLConfig::GetStereoSound()
-{
-	assert(rootNode);
-
-	string resStr = GetNodeText(FindFirstByName(FindFirstByName(FindFirstByName(rootNode, "global"),"sound"),"stereo"));
-	
-	if (resStr[0]=='Y' || resStr[0]=='y')
-		return true;
-	else 
-		return false;
-}
-
-string KD_XMLConfig::GetArtDirectory()
-{
-  assert(rootNode);
-  string resStr = GetNodeText(FindFirstByName(FindFirstByName(rootNode, "art"),"base_directory"));
-  if (resStr[resStr.size()]!= '/') resStr= resStr+ '/';
-  
-  return resStr;
+    if (!xmlFileName.IsArchived())
+    {
+        xmlSaveFormatFile (xmlFileName.GetFullPath().c_str(), configDocument, 1);
+        return true;
+    }
+    else
+        return false;
 }
--- krystaldrop/Sources/KDpp/Tools/XMLConfig.h
+++ krystaldrop/Sources/KDpp/Tools/XMLConfig.h
@@ -3,101 +3,92 @@
 
 #include "defines.h"
 
-extern "C" {
+extern "C"
+{
 #include <libxml/xmlmemory.h>
 #include <libxml/parser.h>
-//#include <libxml/parser.h>
 }
 
 #include "FilePath.h"
 
 /**
-	Reads the XML config file for the application.
+    Reads the XML config file for the application.
   */
 class DllExport KD_XMLConfig
 {
-	/// XML Document
-	xmlDocPtr configDocument;
+    /// XML Document
+    xmlDocPtr configDocument;
 
-	/// Root node
-	xmlNodePtr rootNode;
-
+    /// Root node
+    xmlNodePtr rootNode;
 
 public:
-	/**
-		Default constructor.
-	*/
-	KD_XMLConfig();
+    /**
+        Default constructor.
+    */
+    KD_XMLConfig();
 
-	/**
-		Destructor, it frees the space allocated for the file.
-	*/
-	virtual ~KD_XMLConfig();
+    /**
+        Destructor, it frees the space allocated for the file.
+    */
+    virtual ~KD_XMLConfig();
 
-	/**
-		Opens the XMLFile named "kdrop.xml" and located in the current user directory or several other cool places in Unix
-	*/
-	bool Load();
+    /**
+        Opens the XMLFile named "kdrop.xml" and located in the current user directory or several other cool places in Unix
+    */
+    bool Load();
 
-	/**
-		Opens the XMLFile named fileName.
-	*/
-	bool Load(const KD_FilePath &xmlFileName);
+    /**
+        Opens the XMLFile named fileName.
+    */
+    bool Load(const KD_FilePath &xmlFileName);
 
-	/**
-		Closes the XMLFile.
-	*/
-	bool Unload();
+    /**
+        Closes the XMLFile.
+    */
+    bool Unload();
 
-	/**
-		Returns the XML document
-	*/
-	xmlDocPtr GetConfigDocument();
+    /**
+        Returns the XML document
+    */
+    xmlDocPtr GetConfigDocument() const;
 
-	/**
-		Returns the root node
-	*/
-	xmlNodePtr GetRootNode();
+    /**
+        Returns the root node
+    */
+    xmlNodePtr GetRootNode() const;
 
-	/**
-		Returns the first element in the node "node" with the name "name"
-	*/
-	static xmlNodePtr FindFirstByName(xmlNodePtr node, string name);
+    /**
+        Returns the first element in the node "node" with the name "name"
+    */
+    static xmlNodePtr FindFirstByName(xmlNodePtr node, string name);
 
-	/**
-		Returns the text from the specified node
-	*/
-	string GetNodeText(xmlNodePtr node);
+    /**
+        Returns the text from the specified node
+    */
+    string GetNodeText(xmlNodePtr node) const;
 
-	/**
-		Returns the given attribute value of the given node.
-	*/
-	string GetAttributeFromNode(xmlNodePtr xmlnode, const string &name);
+    /**
+        Returns the given attribute value of the given node.
+    */
+    string GetAttributeFromNode(xmlNodePtr xmlnode, const string &name) const;
 
-	/**
-		Sets the given attribute value of the given node.
-	*/
-	bool SetAttributeFromNode(xmlNodePtr xmlnode, const string &name, const string &value);
+    /**
+        Sets the given attribute value of the given node.
+    */
+    bool SetAttributeFromNode(xmlNodePtr xmlnode, const string &name, const string &value);
 
-	/**
-		Saves the file
-	*/
-	bool Save(const KD_FilePath &xmlFileName);
+    /**
+        Gets a configuration item as a string. Items are specified as 'a/b/../c'
+        Warning, reading from the Config XML file is slow. Values should be read
+        only once and stored somewhere.
+    */
+    string GetConfigurationString (string config_item) const;
 
-	//{
-	/**
-		Reads from / Writes to the config file the parameter "global/opengl"
-		Warning! Reading from the Config file is slow (XML)
-		The value should be read only once and stored
-	*/
-	bool GetOpenGL();
-	bool GetFullScreen();
-	bool GetEnableSound();
-	int  GetSoundFrequency();
-	int  GetSoundBits();
-	bool GetStereoSound();
-  string GetArtDirectory();
-	//}
+    /**
+        Saves the file
+    */
+    bool Save(const KD_FilePath &xmlFileName);
 };
 
 #endif
