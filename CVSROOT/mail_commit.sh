94e9fd30db852250dfff93324b39b6836239ee17
krys <>
2004-05-08 12:53:17 +0000

    - changed output format

--- CVSROOT/mail_commit.sh
+++ CVSROOT/mail_commit.sh
@@ -1,11 +1,10 @@
+#!/bin/sh
 rm -f /tmp/kd_log.txt
-echo "" > /tmp/kd_log.txt
-echo %{sVv} >> /tmp/kd_log.txt
+echo -e -n "\n Commit from ${USER} on " > /tmp/kd_log.txt
 date >> /tmp/kd_log.txt
 echo "" >> /tmp/kd_log.txt
 cat >> /tmp/kd_log.txt
-echo "" >> /tmp/kd_log.txt
-echo "--\n${USER}" >> /tmp/kd_log.txt
+echo -e "\n--\ncvs-${USER}" >> /tmp/kd_log.txt
 /bin/mail -s "[KrystalDrop] Commit" krys@users.sourceforge.net < /tmp/kd_log.txt
 rm -f /tmp/kd_log.txt
 
