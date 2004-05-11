4aa1b8f116311ca4b7275b0dc8d650b7f0595148
krys <>
2004-05-11 20:27:11 +0000

    - better error message in Load() (added the filename) - added the SWAP32 macro to help writing portable code   Note: this macro should be moved to a utility include file

--- krystaldrop/Sources/KDp2p/P2P/AllPeers.cpp
+++ krystaldrop/Sources/KDp2p/P2P/AllPeers.cpp
@@ -4,11 +4,21 @@
 
 #include <stdio.h>
 
+// Note : this macro should be moved somewhere else
+#define SWAP32(x)                                     \
+{ unsigned int i1, i2, i3, i4;                        \
+  c1 = i & 0xFF;                                      \
+  c2 = i & 0xFF00;                                    \
+  c3 = i & 0xFF0000;                                  \
+  c4 = i & 0xFF000000;                                \
+  x = (c1 << 24) | (c2 << 8) | (c3 >> 8) | (c4 >> 24) \
+}
+
 KDp2p_AllPeers::KDp2p_AllPeers()
 {
-	itByTime = peerListByTime.rbegin();
-	//itByTime = peerListByTime.end();
-	//itByTime--;
+    itByTime = peerListByTime.rbegin();
+    //itByTime = peerListByTime.end();
+    //itByTime--;
 }
 
 KDp2p_AllPeers::~KDp2p_AllPeers()
@@ -18,153 +28,163 @@ KDp2p_AllPeers::~KDp2p_AllPeers()
 
 bool KDp2p_AllPeers::IsAPeer(KDp2p_NetworkAddress *address)
 {
-	if (peerListByIP.find(*address) != peerListByIP.end())
-		return true;
-	else
-		return false;
+    if (peerListByIP.find(*address) != peerListByIP.end())
+        return true;
+    else
+        return false;
 }
 
 void KDp2p_AllPeers::AddPeer(KDp2p_NetworkAddress *address)
 {
-	map<KDp2p_NetworkAddress, time_t>::iterator it = peerListByIP.find(*address);
-	// if the peer already exists, lets first delete it in the multimap
-	if (it != peerListByIP.end())
-	{
-		time_t t_old = it->second;
-		multimap<time_t, KDp2p_NetworkAddress>::iterator it2 = peerListByTime.find(t_old);
-		while (!(it2->second == *address))
-		{
-			it2++;
-			if (it2 == peerListByTime.end())
-				throw new KDp2p_NetException("Date not found in list!!! This should never happen!");
-		}
-		peerListByTime.erase(it2);
-	}
+    map<KDp2p_NetworkAddress, time_t>::iterator it = peerListByIP.find(*address);
+    // if the peer already exists, lets first delete it in the multimap
+    if (it != peerListByIP.end())
+    {
+        time_t t_old = it->second;
+        multimap<time_t, KDp2p_NetworkAddress>::iterator it2 = peerListByTime.find(t_old);
+        while (!(it2->second == *address))
+        {
+            it2++;
+            if (it2 == peerListByTime.end())
+                throw new KDp2p_NetException("Date not found in list!!! This should never happen!");
+        }
+        peerListByTime.erase(it2);
+    }
 
-	time_t t;
-	time(&t);
+    time_t t;
+    time(&t);
 
-	peerListByIP[*address] = t;
-	peerListByTime.insert(pair<time_t, KDp2p_NetworkAddress> (t, *address));
+    peerListByIP[*address] = t;
+    peerListByTime.insert(pair<time_t, KDp2p_NetworkAddress> (t, *address));
 
-	/*itByTime = peerListByTime.end();
-	itByTime--;*/
-	itByTime = peerListByTime.rbegin();
+    /*itByTime = peerListByTime.end();
+    itByTime--;*/
+    itByTime = peerListByTime.rbegin();
 }
 
 KDp2p_NetworkAddress KDp2p_AllPeers::GetPeer()
 {
-	return itByTime->second;
+    return itByTime->second;
 }
 
 bool KDp2p_AllPeers::Next()
 {
-	/*itByTime--;
-	if (itByTime == peerListByTime.begin())
-		return false;
-	else
-		return true;*/
-	itByTime++;
-	if (itByTime == peerListByTime.rend())
-		return false;
-	else
-		return true;
+    /*itByTime--;
+    if (itByTime == peerListByTime.begin())
+        return false;
+    else
+        return true;*/
+    itByTime++;
+    if (itByTime == peerListByTime.rend())
+        return false;
+    else
+        return true;
 }
 
 void KDp2p_AllPeers::ResetPointer()
 {
-	/*itByTime = peerListByTime.end();
-	itByTime--;*/
-	itByTime = peerListByTime.rbegin();
+    /*itByTime = peerListByTime.end();
+    itByTime--;*/
+    itByTime = peerListByTime.rbegin();
 }
 
 void KDp2p_AllPeers::Save(const string &filename)
 {
-	FILE *fpt = fopen(filename.c_str(), "wb");
-	if (fpt==0)
-	{
-		throw KDp2p_NetException("Unable to open file in write mode");
-	}
+    FILE *fpt = fopen(filename.c_str(), "wb");
+    if (fpt==0)
+    {
+        throw KDp2p_NetException("Unable to open file in write mode");
+    }
 
-	//KDp2p_P2PEngine::GetSingeton()->GetVersion()
-	//KDp2p_P2PEngine::GetSingeton()->GetMaxPeerNumberInFile()
-	unsigned int version = 1;
-	unsigned int maxPeerNumber = 5000;
-	unsigned int count = (unsigned int) peerListByTime.size();
+    //KDp2p_P2PEngine::GetSingeton()->GetVersion()
+    //KDp2p_P2PEngine::GetSingeton()->GetMaxPeerNumberInFile()
+    unsigned int version = 1;
+    unsigned int maxPeerNumber = 5000;
+    unsigned int count = (unsigned int) peerListByTime.size();
 
-	char magicNumber[5]="KDAP"; // KD ALL PEERS
+    char magicNumber[5]="KDAP"; // KD ALL PEERS
 
-	fwrite(magicNumber, sizeof(char)*4, 1, fpt);
-	fwrite(&version, sizeof(unsigned int), 1, fpt);
-	fwrite(&count, sizeof(unsigned int), 1, fpt);
+    fwrite(magicNumber, sizeof(char)*4, 1, fpt);
+    fwrite(&version, sizeof(unsigned int), 1, fpt);
+    fwrite(&count, sizeof(unsigned int), 1, fpt);
 
-	multimap<time_t, KDp2p_NetworkAddress>::iterator saveIt = peerListByTime.begin();
-	
-	int nb=0;
+    multimap<time_t, KDp2p_NetworkAddress>::iterator saveIt = peerListByTime.begin();
 
-	while (saveIt != peerListByTime.end() && maxPeerNumber != nb)
-	{
-		saveIt->second.WriteToFile(fpt);
-		fwrite(&(saveIt->first), sizeof(time_t), 1, fpt);
-		saveIt++;
-		nb++;
-	}
+    unsigned int nb = 0;
 
-	fclose(fpt);
+    while (saveIt != peerListByTime.end() && maxPeerNumber != nb)
+    {
+        saveIt->second.WriteToFile(fpt);
+        fwrite(&(saveIt->first), sizeof(time_t), 1, fpt);
+        saveIt++;
+        nb++;
+    }
+
+    fclose(fpt);
 }
 
 void KDp2p_AllPeers::Load(const string &filename)
 {
-	FILE *fpt = fopen(filename.c_str(), "rb");
-	if (fpt==0)
-	{
-		throw KDp2p_NetException("Unable to open file in read mode");
-	}
+    FILE *fpt = fopen(filename.c_str(), "rb");
+    if (fpt==0)
+        throw KDp2p_NetException(
+            "Unable to open file `" + filename + "' in read mode");
 
-	//KDp2p_P2PEngine::GetSingeton()->GetVersion()
-	unsigned int version;
-	unsigned int expectedVersion = 1;
-	unsigned int count;
+    //KDp2p_P2PEngine::GetSingleton()->GetVersion()
+    unsigned int version;
+    unsigned int expectedVersion = 1;
+    unsigned int count;
 
-	char magicNumber[5]; // KD ALL PEERS
-	magicNumber[4]=0;
-	int ret = (int) fread(magicNumber, sizeof(char)*4, 1, fpt);
+    char magicNumber[5]; // KD ALL PEERS
+    magicNumber[4]=0;
+    int ret = (int) fread(magicNumber, 4, 1, fpt);
+#ifdef MSB
+    SWAP32(magicNumber);
+#endif
+    if (ret == 0)
+        throw KDp2p_NetException("Unable to read file `" + filename + "'\n");
 
-	if (ret == 0)
-		throw KDp2p_NetException("Unable to read from file\n");
+    if (strcmp(magicNumber, "KDAP"))
+        throw KDp2p_NetException(filename+ ": Invalid file format\n");
 
-	if (strcmp(magicNumber,"KDAP"))
-		throw KDp2p_NetException("Invalid File Format\n");
+    ret = (int) fread(&version, 4, 1, fpt);
+    if (ret == 0)
+        throw KDp2p_NetException("Unable to read file `" + filename + "'\n");
+#ifdef MSB
+    SWAP32(version);
+#endif
 
-	ret = (int) fread(&version, sizeof(unsigned int), 1, fpt);
-	if (ret == 0)
-		throw KDp2p_NetException("Unable to read from file\n");
+    if (version != expectedVersion)
+        throw KDp2p_NetException(filename+ ": File version not correct\n");
 
-	if (version != expectedVersion)
-		throw KDp2p_NetException("File version not correct\n");
+    ret = (int) fread(&count, 4, 1, fpt);
+    if (ret == 0)
+        throw KDp2p_NetException("Unable to read file `" + filename + "'\n");
+#ifdef MSB
+    SWAP32(count);
+#endif
 
-	ret = (int) fread(&count, sizeof(unsigned int), 1, fpt);
-	if (ret == 0)
-		throw KDp2p_NetException("Unable to read from file\n");
+    for (unsigned int i= 0; i< count; i++)
+    {
+        KDp2p_NetworkAddress address;
+        address.ReadFromFile(fpt);
 
+        unsigned int temp_time;
+        ret = (int) fread(&temp_time, 4, 1, fpt);
+        if (ret == 0)
+            throw KDp2p_NetException("Unable to read from file");
+#ifdef MSB
+        SWAP32(temp_time);
+#endif
 
-	for (unsigned int i=0; i<count; i++)
-	{
-		KDp2p_NetworkAddress address;
-		address.ReadFromFile(fpt);
-	
-		time_t time;
-		ret = (int) fread(&time, sizeof(time_t), 1, fpt);
-		if (ret == 0)
-			throw KDp2p_NetException("Unable to read from file");
+        peerListByIP[address] = (time_t) temp_time;
+        peerListByTime.insert(pair<time_t, KDp2p_NetworkAddress>
+            ((time_t) temp_time, address));
+    }
 
-		peerListByIP[address] = time;
-		peerListByTime.insert(pair<time_t, KDp2p_NetworkAddress> (time, address));
-	}
+    fclose(fpt);
 
-	fclose(fpt);
-
-	//itByTime = peerListByTime.end();
-	//itByTime--;
-	itByTime = peerListByTime.rbegin();
+    //itByTime = peerListByTime.end();
+    //itByTime--;
+    itByTime = peerListByTime.rbegin();
 }
--- krystaldrop/Sources/KDp2p/P2P/AllPeers.h
+++ krystaldrop/Sources/KDp2p/P2P/AllPeers.h
@@ -13,46 +13,46 @@ using namespace std;
 #include <sys/timeb.h>
 
 /**
-	Contains the list of all peers ever contacted, with the last date when they have been contacted
+    Contains the list of all peers ever contacted, with the last date when they have been contacted
 */
 class DllExport KDp2p_AllPeers
 {
 
-	/////// ICI il faut un double SET (pour les indexs).
-	/////// Bon y a pb avec l'opérateur de comparaison... zutouille...
-	/////// On va mettre 2 map, mais faudrait que je prévois un objet index, ça serait cool
-	map<KDp2p_NetworkAddress, time_t> peerListByIP;
-	multimap<time_t, KDp2p_NetworkAddress> peerListByTime;
+    /////// ICI il faut un double SET (pour les indexs).
+    /////// Bon y a pb avec l'opérateur de comparaison... zutouille...
+    /////// On va mettre 2 map, mais faudrait que je prévois un objet index, ça serait cool
+    map<KDp2p_NetworkAddress, time_t> peerListByIP;
+    multimap<time_t, KDp2p_NetworkAddress> peerListByTime;
 
-	multimap<time_t, KDp2p_NetworkAddress>::reverse_iterator itByTime;
+    multimap<time_t, KDp2p_NetworkAddress>::reverse_iterator itByTime;
 
-public:	
-	KDp2p_AllPeers();
-	virtual ~KDp2p_AllPeers();
+public:    
+    KDp2p_AllPeers();
+    virtual ~KDp2p_AllPeers();
 
-	/// Finds if the given address is in the list of our peers
-	bool IsAPeer(KDp2p_NetworkAddress *address);
+    /// Finds if the given address is in the list of our peers
+    bool IsAPeer(KDp2p_NetworkAddress *address);
 
-	/// Adds the peer to the list. If the peer exists, updates it.
-	void AddPeer(KDp2p_NetworkAddress *address);	
+    /// Adds the peer to the list. If the peer exists, updates it.
+    void AddPeer(KDp2p_NetworkAddress *address);    
 
-	/**
-		Returns a peer (classified by date from the most current to the oldest)
-		Pointers are reseted when a Peer is added.
-	*/
-	KDp2p_NetworkAddress GetPeer();
+    /**
+        Returns a peer (classified by date from the most current to the oldest)
+        Pointers are reseted when a Peer is added.
+    */
+    KDp2p_NetworkAddress GetPeer();
 
-	/// Move the pointer to the next peer in the list. Returns false when we reach the end (a reversed iterator is used for this list).
-	bool Next();
+    /// Move the pointer to the next peer in the list. Returns false when we reach the end (a reversed iterator is used for this list).
+    bool Next();
 
-	/// Resets the pointer
-	void ResetPointer();
+    /// Resets the pointer
+    void ResetPointer();
 
-	/// Saves the list of peers
-	void Save(const string &filename);
+    /// Saves the list of peers
+    void Save(const string &filename);
 
-	/// Loads the list of peers
-	void Load(const string &filename);
+    /// Loads the list of peers
+    void Load(const string &filename);
 };
 
 #endif
